84dd7993739f565f0a965565fe77915782a5db21
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-09-28 08:02:09 +1000

    start challenge 1 js

--- reverseWords/reverseWords.js
+++ reverseWords/reverseWords.js
@@ -7,14 +7,23 @@ Examples
 */
 
 function reverseWords(str) {
-  console.log(str);
-  let test =  str.split("").reverse().join(" ");
-  console.log(test)
+  console.log("normal: " + str.length);
+  //   let test =  str.split("").reverse().join(" ");
+  //   console.log("First: " + test)
+  //   test = str.split(" ").reverse().join(" ");
+  //   console.log("Second: " + test)
+// let strResult;
+//   for (let i = 0; i < str.length; i++) {
+//     console.log("str[i]: " + str[i])
+//     strResult += str[i];
 
-}
+//   }
+//   console.log("strResult: " +strResult)
+//   let strSplit = strResult.split(",").reverse().join("")
+//   console.log("strSplit: " + strSplit)
+// }
 
-
-reverseWords('The quick brown fox jumps over the lazy dog.'); //ehT kciuq nworb xof spmuj revo eht yzal .god
-reverseWords('apple'); // elppa
-reverseWords('a b c d'); //a b c d
-reverseWords('double  spaced  words'); //elbuod  decaps  sdrow
+reverseWords("The quick brown fox jumps over the lazy dog."); //ehT kciuq nworb xof spmuj revo eht yzal .god
+reverseWords("apple"); // elppa
+reverseWords("a b c d"); //a b c d
+reverseWords("double  spaced  words"); //elbuod  decaps  sdrow
