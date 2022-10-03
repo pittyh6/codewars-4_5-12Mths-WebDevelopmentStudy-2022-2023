79aacda7418e2984fb69f1b3250f943242aba719
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-03 15:06:40 +1100

    arrayDiff - done

--- arrayDiff/array_diff.js
+++ arrayDiff/array_diff.js
@@ -5,6 +5,7 @@ which subtracts one list from another and returns the result.
 It should remove all values from list a, which are present in list b keeping their order.
 */
 function arrayDiff(a, b) {
+    
     for(let i = 0; i < a.length; i++){
         if(b.length <=0){
             return a;
@@ -21,11 +22,32 @@ function arrayDiff(a, b) {
     }
     console.log("final result: " + a)
     return a;
+    
+
+    /*
+   console.log(a.filter(x=> !b.includes(x)))
+   return a.filter(x=> !b.includes(x))
+   */
 }
 
-//arrayDiff([1,2], [1])//, [2], "a was [1,2], b was [1]"
-//arrayDiff([1,2,2], [1])//, [2,2], "a was [1,2,2], b was [1]");
-//arrayDiff([1,2,2], [2])//, [1], "a was [1,2,2], b was [2]");
-//arrayDiff([1,2,2], [])//, [1,2,2], "a was [1,2,2], b was []");
+arrayDiff([1,2], [1])//, [2], "a was [1,2], b was [1]"
+arrayDiff([1,2,2], [1])//, [2,2], "a was [1,2,2], b was [1]");
+arrayDiff([1,2,2], [2])//, [1], "a was [1,2,2], b was [2]");
+arrayDiff([1,2,2], [])//, [1,2,2], "a was [1,2,2], b was []");
 arrayDiff([], [1,2])//, [], "a was [], b was [1,2]");
-//arrayDiff([1,2,3], [1,2])//, [3], "a was [1,2,3], b was [1,2]")
+arrayDiff([1,2,3], [1,2])//, [3], "a was [1,2,3], b was [1,2]")
+
+
+//OTHERS RESULT
+/*
+function array_diff(a, b) {
+  return a.filter(e => !b.includes(e));
+}
+function array_diff(a, b) {
+  return a.filter(function(x) { return b.indexOf(x) == -1; });
+}
+function array_diff(a, b) {
+  b = new Set(b)
+  return a.filter(v => !b.has(v))
+}
+*/
